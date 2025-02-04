f3cbb3e708bfe7d55f9b44a8cd3cfc1c25e1ce01
Hans Løken <hans@ebamse.net>
2025-02-04 09:12:13 +0100

    added ignoredependencies to gitextensions

--- InstallUpdateDevelopmentMachine.ps1
+++ InstallUpdateDevelopmentMachine.ps1
@@ -59,7 +59,7 @@ Write-Host "Installing/upgrading development tools from Chocolatey" -foregroundc
 choco upgrade nuget.commandline -y
 choco upgrade sysinternals -y
 choco upgrade git -y
-choco upgrade gitextensions -y
+choco install gitextensions --ignore-dependencies -y
 choco upgrade gh -y
 choco upgrade poshgit -y
 #choco upgrade beyondcompare -y
